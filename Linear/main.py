2bc6800a7181f951b59e3575952c67d379da31f6
Andrey <andrey.a-petrovskiy@yandex.ru>
2024-11-15 12:25:35 +0300

    lab 3 100%

--- Linear/main.py
+++ Linear/main.py
@@ -7,19 +7,33 @@ import random
 import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter1d
 
-import numpy as np
-
-
 
 class SVM:
-    def __init__(self, kernel='poly', C=10.0, max_iter=1000, degree=3, gamma=1):
+    def __init__(self, C=100.0, max_iter=100, kernel='linear', learning_rate=0.001, gamma=1,degree=5):
+        self.learning_rate = learning_rate
+        self.max_iter = max_iter
         self.kernel_type = kernel
         self.C = C
-        self.max_iter = max_iter
         self.degree = degree
         self.gamma = gamma
 
-    # Метод для вычисления ядра
+
+    def fit(self, x, y):
+        self.X, self.y = x, y
+        self.b = 0
+        n, p = self.X.shape
+        self.alpha = np.zeros(n)
+
+        kernel_matrix = self.kernel(self.X, self.X)
+        for i in range(self.max_iter):
+            for j in range(n):
+                self.alpha[j] -= self.learning_rate * (np.sum(self.alpha * y * kernel_matrix.T[i]) - 1)
+                self.alpha[j] = max(0, min(self.alpha[j], self.C)) # 0 <= alpha[i] <= C
+
+        diff = y - np.dot(kernel_matrix, (self.alpha * y))
+        self.b = sum(diff)/ len(diff)
+
+
     def kernel(self, x, y):
         if self.kernel_type == 'linear':
             return np.dot(x, y.T)
@@ -30,83 +44,9 @@ class SVM:
         else:
             raise ValueError("Неизвестное ядро")
 
-    def fit(self, X, y):
-        self.X_train = X
-        self.labels = y
-        self.alpha = np.zeros_like(self.labels, dtype=float)
-        self.b = 0
-        kernel_matrix = self.kernel(self.X_train, self.X_train) * self.labels.reshape(-1, 1) * self.labels
+    def predict(self, x):
+        return np.sign(np.dot(self.kernel(x, self.X), (self.alpha * self.y)) + self.b)
 
-        n_samples = len(self.labels)
-        iterations = 0
-        while iterations < self.max_iter:
-            alpha_pairs_changed = 0
-            for i in range(n_samples):
-                Ei = np.dot(self.alpha * self.labels, kernel_matrix[i]) + self.b - self.labels[i]
-                if (self.labels[i] * Ei < -1e-3 and self.alpha[i] < self.C) or (
-                        self.labels[i] * Ei > 1e-3 and self.alpha[i] > 0):
-                    j = np.random.randint(0, n_samples)
-                    while j == i:
-                        j = np.random.randint(0, n_samples)
-
-                    Ej = np.dot(self.alpha * self.labels, kernel_matrix[:, j]) + self.b - self.labels[j]
-
-                    alpha_i_old = self.alpha[i]
-                    alpha_j_old = self.alpha[j]
-
-                    if self.labels[i] != self.labels[j]:
-                        L = max(0, self.alpha[j] - self.alpha[i])
-                        H = min(self.C, self.C + self.alpha[j] - self.alpha[i])
-                    else:
-                        L = max(0, self.alpha[i] + self.alpha[j] - self.C)
-                        H = min(self.C, self.alpha[i] + self.alpha[j])
-
-                    if L == H:
-                        continue
-
-                    eta = 2 * kernel_matrix[i, j] - kernel_matrix[i, i] - kernel_matrix[j, j]
-                    if eta >= 0:
-                        continue
-
-                    self.alpha[j] -= (self.labels[j] * (Ei - Ej)) / eta
-                    self.alpha[j] = np.clip(self.alpha[j], L, H)
-
-                    if np.abs(self.alpha[j] - alpha_j_old) < 1e-5:
-                        continue
-
-                    self.alpha[i] += self.labels[i] * self.labels[j] * (alpha_j_old - self.alpha[j])
-
-                    b1 = self.b - Ei - self.labels[i] * (self.alpha[i] - alpha_i_old) * kernel_matrix[i, i] - \
-                         self.labels[j] * (self.alpha[j] - alpha_j_old) * kernel_matrix[i, j]
-                    b2 = self.b - Ej - self.labels[i] * (self.alpha[i] - alpha_i_old) * kernel_matrix[i, j] - \
-                         self.labels[j] * (self.alpha[j] - alpha_j_old) * kernel_matrix[j, j]
-
-                    if 0 < self.alpha[i] < self.C:
-                        self.b = b1
-                    elif 0 < self.alpha[j] < self.C:
-                        self.b = b2
-                    else:
-                        self.b = (b1 + b2) / 2
-
-                    alpha_pairs_changed += 1
-
-            iterations += 1
-            if alpha_pairs_changed == 0:
-                break
-
-        # Определяем индексы опорных векторов
-        support_indices = np.where(self.alpha > 1E-15)[0]
-        # Вычисляем свободный член b
-        self.b = np.mean((1 - np.sum(kernel_matrix[support_indices] * self.alpha, axis=1)) * self.labels[support_indices])
-
-    def decision_function(self, X):
-        kernel_eval = self.kernel(X, self.X_train)
-        return np.dot(kernel_eval * self.labels, self.alpha) + self.b
-
-    def predict(self, X):
-        # Предсказываем метки классов
-        predictions = np.sign(self.decision_function(X))
-        return predictions
 
 
 class GradientLinearClassifier:
@@ -198,108 +138,6 @@ X = X.drop(X.columns[43:-1], axis=1)  # Удаляем favorite champions
 X = X.drop(X.columns[37:-1], axis=1)  # Удаляем favorite roles
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7, random_state=42)
-
-# classifierGrad = GradientLinearClassifier()
-# classifierGrad.fit(X_train.values, y_train.values)
-# y_pred1 = classifierGrad.predict(X_test.values)
-# accuracy1 = accuracy_score(y_test, y_pred1)
-# print(f"Accuracy gradient: {accuracy1:.2f}")
-#
-# classifierMatrix = MatrixLinearClassifier()
-# classifierMatrix.fit(X_train.values, y_train.values)
-
-
-def random_search_svm(X_train, y_train, n_iter=50):
-    # Диапазоны гиперпараметров для случайного перебора
-    C_range = [0.1, 1, 10, 100]
-    degree_range = [2, 3, 4, 5]
-    kernel_options = ['linear', 'poly', 'rbf']
-    gamma_range = [0.01, 0.1, 1, 10]
-
-    best_accuracy = 0
-    best_params = None
-
-    for i in range(n_iter):
-        # Случайный выбор гиперпараметров
-        C = random.choice(C_range)
-        degree = random.choice(degree_range)
-        kernel = random.choice(kernel_options)
-        gamma = random.choice(gamma_range)
-
-        # Инициализируем и обучаем SVM
-        classifier = SVM(kernel=kernel, C=C, degree=degree, gamma=gamma)
-        classifier.fit(X_train.values, y_train.values)
-        y_pred = classifier.predict(X_train.values)
-        accuracy = accuracy_score(y_train, y_pred)
-
-        # Обновляем лучшие гиперпараметры, если текущая точность выше
-        if accuracy > best_accuracy:
-            best_accuracy = accuracy
-            best_params = (C, degree, kernel, gamma)
-        print(f"{i + 1} random_search_svm")
-
-    print(f"Лучшие гиперпараметры SVM: C={best_params[0]}, degree={best_params[1]}, kernel={best_params[2]}, gamma={best_params[3]}")
-    print(f"Лучшая точность на обучении: {best_accuracy:.2f}")
-    return best_params
-
-
-def random_search_gradient(X_train, y_train, n_iter=50):
-    # Диапазоны гиперпараметров для случайного перебора
-    learning_rate_range = [0.0001, 0.001, 0.01, 0.1]
-    alpha_range = [0.1, 0.5, 1, 5]
-    lambda_range = [0, 0.1, 0.5, 1]
-
-    best_accuracy = 0
-    best_params = None
-
-    for _ in range(n_iter):
-        # Случайный выбор гиперпараметров
-        learning_rate = random.choice(learning_rate_range)
-        alpha = random.choice(alpha_range)
-        lambda_ = random.choice(lambda_range)
-
-        # Инициализируем и обучаем градиентный классификатор
-        classifier = GradientLinearClassifier(learning_rate=learning_rate, alpha=alpha, lambda_=lambda_)
-        classifier.fit(X_train.values, y_train.values)
-        y_pred = classifier.predict(X_train.values)
-        accuracy = accuracy_score(y_train, y_pred)
-
-        # Обновляем лучшие гиперпараметры, если текущая точность выше
-        if accuracy > best_accuracy:
-            best_accuracy = accuracy
-            best_params = (learning_rate, alpha, lambda_)
-        print(f"{i + 1} random_search_gradient")
-    print(f"Лучшие гиперпараметры Gradient Classifier: learning_rate={best_params[0]}, alpha={best_params[1]}, lambda_={best_params[2]}")
-    print(f"Лучшая точность на обучении: {best_accuracy:.2f}")
-    return best_params
-
-
-# best_svm_params = random_search_svm(X_train, y_train, n_iter=100)
-# best_gradient_params = random_search_gradient(X_train, y_train, n_iter=100)
-
-# Обучаем модели с лучшими гиперпараметрами
-classifierGrad = GradientLinearClassifier()
-
-classifierSVM = SVM()
-
-classifierMatrix = MatrixLinearClassifier()
-
-# Обучаем модели и проверяем точность
-classifierGrad.fit(X_train.values, y_train.values)
-y_pred_grad = classifierGrad.predict(X_test.values)
-accuracy_grad = accuracy_score(y_test, y_pred_grad)
-print(f"Точность Gradient Classifier на тесте: {accuracy_grad:.2f}")
-
-classifierSVM.fit(X_train.values, y_train.values)
-y_pred_svm = classifierSVM.predict(X_test.values)
-accuracy_svm = accuracy_score(y_test, y_pred_svm)
-print(f"Точность SVM на тесте: {accuracy_svm:.2f}")
-
-classifierMatrix.fit(X_train.values, y_train.values)
-y_pred_matrix = classifierMatrix.predict(X_test.values)
-accuracy_matrix = accuracy_score(y_test, y_pred_matrix)
-print(f"Точность Matrix Classifier на тесте: {accuracy_matrix:.2f}")
-
 
 def plot_learning_curve(classifier, X, y):
 
@@ -405,17 +243,108 @@ def plot_test_error_curve_with_baseline(classifier, X, y, n_splits=5):
     plt.legend()
     plt.show()
 
+def random_search_svm(X_train, y_train, n_iter=50):
+    # Диапазоны гиперпараметров для случайного перебора
+    C_range = [0.1, 1, 10, 100]
+    degree_range = [2, 3, 4, 5]
+    kernel_options = ['linear', 'poly', 'rbf']
+    gamma_range = [0.01, 0.1, 1, 10]
 
-# print("Построение кривой обучения для GradientLinearClassifier...")
-# plot_learning_curve(classifierGrad, X_train, y_train)
-#
-# print("Построение кривой обучения для SVM...")
-# plot_learning_curve(classifierSVM, X_train, y_train)
-#
-# print("Построение кривой ошибки на тестовом множестве для GradientLinearClassifier...")
-# plot_test_error_curve(classifierGrad, X, y)
-#
-# print("Построение кривой ошибки на тестовом множестве для SVM...")
-# plot_test_error_curve(classifierSVM, X, y)
+    best_accuracy = 0
+    best_params = None
+
+    for i in range(n_iter):
+        # Случайный выбор гиперпараметров
+        C = random.choice(C_range)
+        degree = random.choice(degree_range)
+        kernel = random.choice(kernel_options)
+        gamma = random.choice(gamma_range)
+
+        # Инициализируем и обучаем SVM
+        classifier = SVM(kernel=kernel, C=C, degree=degree, gamma=gamma)
+        classifier.fit(X_train.values, y_train.values)
+        y_pred = classifier.predict(X_train.values)
+        accuracy = accuracy_score(y_train, y_pred)
+
+        # Обновляем лучшие гиперпараметры, если текущая точность выше
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            best_params = (C, degree, kernel, gamma)
+        print(f"{i + 1} random_search_svm")
+
+    print(f"Лучшие гиперпараметры SVM: C={best_params[0]}, degree={best_params[1]}, kernel={best_params[2]}, gamma={best_params[3]}")
+    print(f"Лучшая точность на обучении: {best_accuracy:.2f}")
+    return best_params
+
+
+def random_search_gradient(X_train, y_train, n_iter=50):
+    # Диапазоны гиперпараметров для случайного перебора
+    learning_rate_range = [0.0001, 0.001, 0.01, 0.1]
+    alpha_range = [0.1, 0.5, 1, 5]
+    lambda_range = [0, 0.1, 0.5, 1]
+
+    best_accuracy = 0
+    best_params = None
+
+    for i in range(n_iter):
+        # Случайный выбор гиперпараметров
+        learning_rate = random.choice(learning_rate_range)
+        alpha = random.choice(alpha_range)
+        lambda_ = random.choice(lambda_range)
+
+        # Инициализируем и обучаем градиентный классификатор
+        classifier = GradientLinearClassifier(learning_rate=learning_rate, alpha=alpha, lambda_=lambda_)
+        classifier.fit(X_train.values, y_train.values)
+        y_pred = classifier.predict(X_train.values)
+        accuracy = accuracy_score(y_train, y_pred)
+
+        # Обновляем лучшие гиперпараметры, если текущая точность выше
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            best_params = (learning_rate, alpha, lambda_)
+        print(f"{i + 1} random_search_gradient")
+    print(f"Лучшие гиперпараметры Gradient Classifier: learning_rate={best_params[0]}, alpha={best_params[1]}, lambda_={best_params[2]}")
+    print(f"Лучшая точность на обучении: {best_accuracy:.2f}")
+    return best_params
+
+# Лучшие гиперпараметры SVM: C=0.1, degree=4, kernel=rbf, gamma=1
+best_svm_params = random_search_svm(X_train, y_train, n_iter=100)
+# Лучшие гиперпараметры Gradient Classifier: learning_rate=0.1, alpha=0.1, lambda_=0
+best_gradient_params = random_search_gradient(X_train, y_train, n_iter=100)
+
+
+classifierGrad = GradientLinearClassifier()
+classifierSVM = SVM()
+classifierMatrix = MatrixLinearClassifier()
+
+classifierGrad.fit(X_train.values, y_train.values)
+y_pred_grad = classifierGrad.predict(X_test.values)
+accuracy_grad = accuracy_score(y_test, y_pred_grad)
+print(f"Точность Gradient Classifier на тесте: {accuracy_grad:.2f}")
+
+
+classifierSVM.fit(X_train.values, y_train.values)
+y_pred_svm = classifierSVM.predict(X_test.values)
+accuracy_svm = accuracy_score(y_test, y_pred_svm)
+print(f"Точность SVM на тесте: {accuracy_svm:.2f}")
+
+classifierMatrix.fit(X_train.values, y_train.values)
+y_pred_matrix = classifierMatrix.predict(X_test.values)
+accuracy_matrix = accuracy_score(y_test, y_pred_matrix)
+print(f"Точность Matrix Classifier на тесте: {accuracy_matrix:.2f}")
+
+
+print("Построение кривой обучения для GradientLinearClassifier...")
+plot_learning_curve(classifierGrad, X_train, y_train)
+
+print("Построение кривой обучения для SVM...")
+plot_learning_curve(classifierSVM, X_train, y_train)
+
+print("Построение кривой ошибки на тестовом множестве для GradientLinearClassifier...")
+plot_test_error_curve(classifierGrad, X, y)
+
+print("Построение кривой ошибки на тестовом множестве для SVM...")
+plot_test_error_curve(classifierSVM, X, y)
+
 print("Построение кривой ошибки на тестовом множестве для MatrixLinearClassifier...")
 plot_test_error_curve_with_baseline(classifierMatrix, X, y)
